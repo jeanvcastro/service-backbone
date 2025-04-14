e54f161613e8b5552b1cdd216b07b7ba186cca49
jeanvcastro <jean.castro1@outlook.com>
2025-04-14 14:51:45 -0300

    style: add prettier and editorconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
 import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
+  eslintConfigPrettier
 ]);

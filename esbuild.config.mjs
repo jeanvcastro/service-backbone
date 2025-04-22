cc30ef2e8214a102d1c15eadc046cdcfb9da1924
jeanvcastro <jean.castro1@outlook.com>
2025-04-22 09:38:51 -0300

    refactor: update build configuration to use CommonJS format and adjust TypeScript settings

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -7,19 +7,8 @@ build({
   entryPoints: ["src/infra/http/express/app.ts"],
   outdir: "dist",
   platform: "node",
-  target: "node22",
-  format: "esm",
+  format: "cjs",
   sourcemap: true,
   bundle: true,
-  banner: {
-    js: `
-      import { createRequire } from "node:module";
-      import { fileURLToPath } from "node:url";
-      import * as pathModule from "node:path";
-      const require = createRequire(import.meta.url);
-      const __filename = fileURLToPath(import.meta.url);
-      const __dirname  = pathModule.dirname(__filename);
-    `
-  },
   external: ["better-sqlite3", "mysql", "mysql2", "oracledb", "pg-query-stream", "sqlite3", "tedious"]
 }).catch(() => process.exit(1));
